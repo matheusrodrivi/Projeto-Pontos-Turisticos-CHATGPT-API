c9477119677920740928bdb22e569a12902da14f
Matheus Rodrigues <matheusrovicontato@gmail.com>
2023-09-14 13:52:57 -0300

    atualização

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const formulario = document.getElementById('formulario')
 const OPENAI_API_KEY = "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";
+corHora();
 
 if (formulario){
 
@@ -78,15 +79,17 @@ if (formulario){
         .catch (() => {
             document.getElementById('resposta2').innerHTML = 'Sem resposta'; 
         });
+        document.getElementById('botao').value = "Descobrir";
     });
 }
 
+//Função da cor de fundo padrão ao abrir o site // aplicativo
 function corHora(){
 
     var currentTime = new Date().getHours();
-    if (13 <= currentTime && currentTime < 22) {
+    if (6 <= currentTime && currentTime < 7) {
 
-        document.body.style.backgroundColor = "red";
+        document.body.style.backgroundColor = "white";
 
     }else if (7 <= currentTime && currentTime < 8) {
 
@@ -107,21 +110,86 @@ function corHora(){
     }else if (11 <= currentTime && currentTime < 12) {
             
         document.body.style.backgroundColor = "#FFFACD";
+    }else if (13 <= currentTime && currentTime < 14) {
 
-    }         
+        document.body.style.backgroundColor = "white";
+
+    }else if (15 <= currentTime && currentTime < 16) {
+
+        document.body.style.backgroundColor = "aliceblue";
+
+    }else if (17 <= currentTime && currentTime < 18) {
+
+        document.body.style.backgroundColor = "#E6E6FA";
+        
+    }else if (19 <= currentTime && currentTime < 20) {
+
+        document.body.style.backgroundColor = "#E0FFFF";
+        
+    }else if (21 <= currentTime && currentTime < 22) {
+
+        document.body.style.backgroundColor = "#FFFFF0";
+
+    }else if (23 <= currentTime && currentTime < 24) {
+                
+        document.body.style.backgroundColor = "#FFFACD";
+
+    }else if (0 <= currentTime && currentTime < 1) {
+
+        document.body.style.backgroundColor = "white";
+
+    }else if (1 <= currentTime && currentTime < 2) {
+
+        document.body.style.backgroundColor = "aliceblue";
+
+    }else if (2 <= currentTime && currentTime < 3) {
+            
+        document.body.style.backgroundColor = "#E6E6FA";
+
+    }else if (3 <= currentTime && currentTime < 4) {
+            
+        document.body.style.backgroundColor = "#E0FFFF";
+
+    }else if (4 <= currentTime && currentTime < 5) {
+                
+        document.body.style.backgroundColor = "#FFFFF0";
+
+    }else if (5 <= currentTime && currentTime < 6) {
+                        
+        document.body.style.backgroundColor = "#FFFACD";
+    }   
+}
+
+//Função que altera a cor ao clicar no botão
+function corBotao(){
+
+    var numero = Math.floor(Math.random() * 5) + 1;
+    
+    if (numero == 1) {
+        document.body.style.backgroundColor = "blue";
+    }else if (numero == 2) {
+        document.body.style.backgroundColor = "red";
+    }else if (numero == 3) {
+        document.body.style.backgroundColor = "green";
+    }else if (numero == 4) {
+        document.body.style.backgroundColor = "yellow";
+    }else if (numero == 5) {
+        document.body.style.backgroundColor = "purple";
+    }
+
 }
 
 if (formulario){
     formulario.addEventListener("submit", async (e) => {
 
         e.preventDefault();
-        corHora();
+        corBotao();
 
         const data = new Date();
         const dia = String(data.getDate()).padStart(2, '0');
         const mes = String(data.getMonth() + 1).padStart(2, '0');
         const ano = data.getFullYear();
-
+        
         document.getElementById('resposta3').innerHTML = 'Esses dados foram gerados no dia ' + dia + '/' + mes + '/' + ano;
 
     });
